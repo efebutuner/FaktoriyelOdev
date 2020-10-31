0c994afa91d753a9e5ab91c3076abcd84a8f54ef
Efe Bütüner <efebut@gmail.com>
2020-10-31 20:21:08 +0300

    v1

--- FaktoriyelOdev/Program.cs
+++ FaktoriyelOdev/Program.cs
@@ -10,6 +10,27 @@ namespace FaktoriyelOdev
     {
         static void Main(string[] args)
         {
+            Console.Write("Lütfen faktöryelini hesaplamak istediğiniz sayıyı giriniz: ");
+            string faktoryel = Console.ReadLine();
+            int fakto = int.Parse(faktoryel);
+            if (fakto > 0)
+            {
+                long faktoryelsonuc = 1;
+                for (int i = fakto; i >= 1; i--)
+                {
+                    faktoryelsonuc *= i;
+                }
+                Console.WriteLine(faktoryelsonuc);
+            }
+            else if (fakto == 0)
+            {
+                Console.WriteLine("1");
+            }
+            else
+            {
+                Console.WriteLine("Negatif sayıların faktöryeli yoktur.");
+            }
+            Console.ReadLine();
         }
     }
 }
